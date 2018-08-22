e9aa295896ac9d8b21391c921dea6a9e4426e5f0
Justas384 <just.vaicius@gmail.com>
2018-08-22 16:40:06 +0300

    Changed project structure, initial app header, sign up and login implementations.

---
+++ src/component/diary/Diary.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Diary extends Component {
+    render() {
+        return (
+            <div>
+                {/*<h3>Diary</h3>*/}
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './component/App';
+import {applyMiddleware, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import promise from 'redux-promise';
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+import reducers from './common/reducer';
+import App from "./app/App";
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
